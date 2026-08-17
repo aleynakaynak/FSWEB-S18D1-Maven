cb81be80dfd644f60ed7c8e83ff856469a62a041
Aleyna <104082194+aleynakaynak@users.noreply.github.com>
2026-08-17 16:03:03 +0300

    Solve SQL exercises: add queries, update ResultAnalyzer userId, add H2 test config; fix projections

--- src/main/java/com/workintech/sqldmljoins/entity/StudentNameSurnameCount.java
+++ src/main/java/com/workintech/sqldmljoins/entity/StudentNameSurnameCount.java
@@ -1,20 +1,7 @@
 package com.workintech.sqldmljoins.entity;
 
-import jakarta.persistence.ColumnResult;
-import jakarta.persistence.ConstructorResult;
-import jakarta.persistence.NamedNativeQuery;
-import jakarta.persistence.SqlResultSetMapping;
-
-@NamedNativeQuery(name="findStudentNameSurnameCount", query = "SELECT o.ad, o.soyad, COUNT(i.kitapno) FROM ogrenci as o " +
-        " INNER JOIN islem as i " +
-        " ON o.ogrno = i.ogrno " +
-        " GROUP BY o.ad, o.soyad" +
-        " GROUP BY o.sinif ", resultSetMapping = "Mapping.KitapCount")
-@SqlResultSetMapping(name="Mapping.KitapCount", classes = @ConstructorResult(targetClass = KitapCount.class,
-        columns = {@ColumnResult(name = "sinif"),
-                @ColumnResult(name = "count")}))
 public interface StudentNameSurnameCount {
-    String getAd();
-    String getSoyad();
-    int getCount();
+        String getAd();
+        String getSoyad();
+        int getCount();
 }
